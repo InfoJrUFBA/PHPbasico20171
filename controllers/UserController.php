f30e64abc655ef734e83bf624d8065c76a04f806
viniciusbj <viniciusbrito@infojr.com.br>
2017-07-28 14:51:46 -0300

    1 questao

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -16,6 +16,17 @@ class UserController {
 
     public static function printUserList(){
         $users = self::getAllUsers();
+        for ($i=0; $i<sizeof($users); $i++) {
+            $menor=$i;
+            for ($j=0; $j<sizeof($users); $j++) {
+                if ($users[$j]->name>$users[$menor]->name) {
+                    $menor=$j;
+                }
+                $aux=$users[$i];
+                $users[$i]=$users[$menor];
+                $users[$menor]=$aux;
+            }
+        }
         foreach ($users as $user) {
             echo nl2br("Nome: ".$user->name."\n"."Idade: ".$user->age."\n\n");
         }
