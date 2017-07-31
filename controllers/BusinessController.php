be47f9cb7f3356d66c51e881e52ce5ace338dcf4
viniciusbj <viniciusbrito@infojr.com.br>
2017-07-31 14:41:02 -0300

    2 questao (e para imprimir ids mesmo?

--- controllers/BusinessController.php
+++ controllers/BusinessController.php
@@ -17,7 +17,7 @@ class BusinessController {
     public static function printBusinessList() {
         $businesss = self::getAllBusinesses();
         foreach ($businesss as $business) {
-            echo nl2br('<img src="'.$business->logo.'" alt="" height="100" width="auto" />'."\n".$business->name."\n\n\n\n");
+            echo nl2br('<img src="'.$business->logo.'" alt="" height="100" width="auto" />'."\n".$business->name."\n"."Dono: ".$business->owners_id."\n\n\n");
         }
     }
 
