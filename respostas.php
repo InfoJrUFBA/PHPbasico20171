97ce10f9a03c1d94cebfd2afde1df9ab61faab05
viniciusbj <viniciusbrito@infojr.com.br>
2017-08-01 19:45:37 -0300

    atividade 6 e correçao atividade 4 e 5

--- respostas.php
+++ respostas.php
@@ -1,26 +1,87 @@
 <?php
 
-	$inteiroA = 2;
-	$inteiroB = 3;
+	$inteiroA = -2;
+	$inteiroB = -3;
+
+    // Funções auxiliares
+
+    // retorna o valor absoluto
+    function vAbs ( $a ) {
+        if ($a<0) {
+            return -$a;
+        } else { 
+            return $a;
+        }
+    }
 
 	// Atividade 4
 
 	function soma ( $a , $b ) { 
-		for ( $i = 0 ; $i < $b ; $i++ ) {
-			$a++;
-		}
+        // inverte para ter o menor número de loops no for
+		if ( vAbs($a) < vAbs($b) ) {
+            $aux=$a;
+            $a=$b;
+            $b=$aux;
+        }
+        // se b for negativo decrementa a, senão encrementa
+        if ($b<0) {
+    		for ( $i = 0 ; $i < vAbs($b) ; $i++ ) {
+    			$a--;
+    		}
+        } else {
+            for ( $i = 0 ; $i < $b ; $i++ ) {
+                $a++;
+            }
+        }
 		return $a;
 	}
 
 	// Atividade 5
 
 	function multiplicacao ( $a , $b ) {
-		for ( $i = 0 ; $i < $b ; $i++ ) {
-			soma($a, $a);
-		}
-		return $a; 
+		// inverte para ter o menor número de loops no for
+        if ( vAbs($a) < vAbs($b) ) {
+            $aux=$a;
+            $a=$b;
+            $b=$aux;
+        }
+        $aux=$a;
+        if ($b<0) {
+    		for ( $i = 1 ; $i < vAbs($b) ; $i++ ) {
+    			$a=soma($a, $aux);
+    		}
+            return -$a;
+        } else {
+            for ( $i = 1 ; $i < $b ; $i++ ) {
+                $a=soma($a, $aux);
+            }
+		    return $a;
+        }
 	}
 
-	echo multiplicacao($inteiroA, $inteiroB);
+    // Atividade 6
+
+    function potencia ( $a , $b ) {
+        $aux=$a;
+        if ($b<0) {
+            for ( $i = 1 ; $i < vAbs($b) ; $i++ ) {
+                $a=multiplicacao($a, $aux);
+            }
+            // como não estou lidando com double, preferi retornar como string para representar uma fração
+            if ($a<0) { 
+                // para o menos ficar no valor de cima
+                return "-1/".vAbs($a);
+            } else {
+                return "1/".$a;
+            }
+        } else {
+            for ( $i = 1 ; $i < $b ; $i++ ) {
+                $a=multiplicacao($a, $aux);
+            }
+            return $a;
+        }
+    }
+
+	echo potencia ($inteiroA, $inteiroB);
 
 ?>
