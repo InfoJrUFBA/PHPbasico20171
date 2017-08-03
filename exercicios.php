78411af73995325a41ae6dd5e19b7a3a8b93d7b2
Vinícius Pinto <viniciuspinto@infojr.com.br>
2017-08-03 07:30:02 -0300

    Adiciona função de multiplicação em "exercicios.php"

--- exercicios.php
+++ exercicios.php
@@ -1,7 +1,7 @@
 <?php 
 	//Declara as variáveis para as operações
-	$num1 = 5646;
-	$num2 = 1165167777;
+	$num1 = 76;
+	$num2 = 6;
 
 	//Cria função que soma 2 numeros naturais
 	function soma ($a, $b){
@@ -22,6 +22,23 @@
 		}
 	}
 
+	//Cria função que multiplica 2 numeros naturais
+	function produto ($a, $b){
+		$result = 0;
+		if ($a>=$b) {
+			for($i=0; $i<$b; $i ++){
+			$result = soma($result,$a);		
+			}
+		}
+		
+		else {
+			for($i=0; $i<$a; $i ++){
+			$result = soma($result,$b);		
+			}
+		}
+		return $result;
+	}
+
 ?>
 
 <!DOCTYPE html>
@@ -35,9 +52,20 @@
 		<p>
 			<?php
 				//Guarda o retorno da fuñção em uma variável
-				$result = soma($num1, $num2);
+				$result1 = soma($num1, $num2);
 				//Exibe o resultado
-				echo "$num1 + $num2 = $result";
+				echo "$num1 + $num2 = $result1";
+		 	?>
+		 	
+		 </p>
+
+		 <h3>Função de Multiplicação</h3>
+		<p>
+			<?php
+				//Guarda o retorno da fuñção em uma variável
+				$result2 = produto($num1, $num2);
+				//Exibe o resultado
+				echo "$num1 x $num2 = $result2";
 		 	?>
 		 	
 		 </p>
