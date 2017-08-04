150630127e7dcbd3f987d5b444e6a2d9854c2890
ViníciusPinto <viniciuspinto@infojr.com.br>
2017-08-04 14:58:05 -0300

    Modifica função 'printUserList' para mostrar em ordem alfabética

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("../models/User.php");
+require_once(__DIR__."/../models/User.php");
 
 class UserController {
 
@@ -16,10 +16,32 @@ class UserController {
 
     public static function printUserList(){
         $users = self::getAllUsers();
+
+        //Guarda o tamanho da array $users na variável $tam
+        $tam = sizeof($users);
+
+        //Aplica um selection sort que ordena de acordo com o nome
+        for ($i=0; $i <($tam-1) ; $i++) { 
+            $min = $i;
+            
+            for($j=($i+1); $j<$tam; $j++) {
+                if($users[$j]->name < $users[$min]->name){
+                    $min = $j;
+                }
+            }
+
+            if($users[$i] != $users[$min]){
+                $aux = $users[$i];
+                $users[$i] = $users[$min];
+                $users[$min] = $aux;
+            }
+        }
+
         foreach ($users as $user) {
             echo nl2br("Nome: ".$user->name."\n"."Idade: ".$user->age."\n\n");
-        }
+        }      
     }
 
 }
+
 ?>
---
+++ exercicios2.php
@@ -0,0 +1,12 @@
+<?php 
+include ("./controllers/UserController.php");
+
+	function ordenaAlf ($lista){
+
+		return 0;
+	}
+	
+	echo nl2br("\n\n");
+	UserController::printUserList();
+
+?>
